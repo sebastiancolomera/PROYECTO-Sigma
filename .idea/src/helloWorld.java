3c686e403dbaf50cb94be3b87a6c3692de26991a
Felipe Rain <felipeysmaelrain@gmail.com>
2026-04-03 17:32:12 -0300

    chore(hello): update greeting message

--- .idea/src/helloWorld.java
+++ .idea/src/helloWorld.java
@@ -1,5 +1,5 @@
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!!!");
+        System.out.println("Hola Sr Zombie, sea puntual");
     }
 }
