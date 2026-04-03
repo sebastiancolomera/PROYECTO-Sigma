6655fecdc34cba8ec0d0d704642285edd3e9ff7f
sebastiancolomera <s.colomera01@ufromail.cl>
2026-04-03 17:22:59 -0300

    Mejora helloWorld en sebastiancolomeraaa.

--- .idea/src/helloWorld.java
+++ .idea/src/helloWorld.java
@@ -1,5 +1,5 @@
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!!!");
+        System.out.println("Hola chat, como estais?? Fernando torres quiere postular a ayudante de poo, es muy integilente, cuando se vaya jesus se puede quedar el.");
     }
 }
